023b6f6bced0a7c9fcd6cd6b3f914eb66fd518d5
Aditria Pardana <aditria.pardana20@gmail.com>
2023-09-25 16:29:48 +0700

    config layout setUp Provider ChakraUI in Root

---
+++ components/Layout.jsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Papanmas Estate</title>
+      </Head>
+      <Box maxWidth="1280px" m="auto">
+        <header>Navbar</header>
+        <main>{children}</main>
+        <footer>Footer</footer>
+      </Box>
+    </>
+  );
+}
+
+export default Layout;
