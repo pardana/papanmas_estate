43bc3d8236a10cf47c2d861dbcf0c63c4be84fb3
Aditria Pardana <aditria.pardana20@gmail.com>
2023-09-25 14:07:26 +0700

    update config and banner

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,63 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Button } from '@chakra-ui/react'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} alt="Banner Image" />
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">
+        {purpose}
+      </Text>
+      <Text fontSize="3xl" fontWeight="bold">
+        {title1}
+        <br />
+        {title2}
+      </Text>
+      <Text color="gray.700" fontSize="xl" paddingTop="3" paddingBottom="3">
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
+      <Button fontSize="xl">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+);
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1><Button>Hello World</Button></h1>
+    <div>
+      <Banner
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Banner
+        purpose="BUY A HOME"
+        title1="Find, Buy & Own Your"
+        title2="Dream Home"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
     </div>
-  )
+  );
 }
