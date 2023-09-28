e577636ede0c6545d89ba3cecdd71403e0f08860
Aditria Pardana <aditria.pardana20@gmail.com>
2023-09-28 16:05:34 +0700

    update image details

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,4 +1,4 @@
-import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Text, Avatar } from "@chakra-ui/react";
 import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
@@ -29,6 +29,107 @@ const PropertyDetails = ({
     <>
       <Box maxWidth="1000px" margin="auto" p={4}>
         {photos && <ImageScrollBar data={photos} />}
+        <Box w="full" p={6}>
+          <Flex
+            paddingTop={2}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Flex alignItems="center">
+              <Box paddingRight={3} color="green.400">
+                {isVerified && <GoVerified />}
+              </Box>
+              <Text fontWeight="bold" fontSize="lg">
+                ${millify(price)}
+                {rentFrequency && `/${rentFrequency}`}
+              </Text>
+            </Flex>
+            <Box>
+              <Avatar size="sm" src={agency?.logo?.url} />
+            </Box>
+          </Flex>
+          <Flex
+            alignItems="center"
+            p={1}
+            justifyContent="space-between"
+            w="250px"
+            color="blue.400"
+          >
+            {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+            <BsGridFill />
+          </Flex>
+          <Box marginTop={2}>
+            <Text fontSize="lg" marginBottom={2} fontWeight="bold">
+              {title}
+            </Text>
+            <Text lineHeight={2} textAlign="justify" color="gray.600">
+              {description}
+            </Text>
+          </Box>
+          <Flex
+            flexWrap="wrap"
+            textTransform="uppercase"
+            justifyContent="space-between"
+          >
+            <Flex
+              w="400px"
+              justifyContent="space-between"
+              borderBottom="1px"
+              borderColor="gray.100"
+              p={3}
+            >
+              <Text>Type</Text>
+              <Text fontWeight="bold">{type}</Text>
+            </Flex>
+            <Flex
+              w="400px"
+              justifyContent="space-between"
+              borderBottom="1px"
+              borderColor="gray.100"
+              p={3}
+            >
+              <Text>Purpose</Text>
+              <Text fontWeight="bold">{purpose}</Text>
+            </Flex>
+            {furnishingStatus && (
+              <Flex
+                w="400px"
+                justifyContent="space-between"
+                borderBottom="1px"
+                borderColor="gray.100"
+                p={3}
+              >
+                <Text>Furnishing Status</Text>
+                <Text fontWeight="bold">{furnishingStatus}</Text>
+              </Flex>
+            )}
+          </Flex>
+          <Box>
+            {amenities.length && (
+              <Text fontSize="2xl" fontWeight="black" marginTop={5}>
+                Amenities
+              </Text>
+            )}
+            <Flex flexWrap="wrap">
+              {amenities.map((item) =>
+                item?.amenities.map((amenity) => (
+                  <Text
+                    fontWeight="bold"
+                    color="blue.400"
+                    fontSize="l"
+                    p={2}
+                    bg="gray.200"
+                    m={1}
+                    borderRadius={5}
+                    key={amenity.text}
+                  >
+                    {amenity.text}
+                  </Text>
+                ))
+              )}
+            </Flex>
+          </Box>
+        </Box>
       </Box>
     </>
   );
